e5f81e91fd08af96c8dee7795ce6058d3ce30262
rex <dsinghm98@gmail.com>
2020-01-22 21:44:08 +0530

    displaying info

--- CSE-B/post.php
+++ CSE-B/post.php
@@ -1,5 +1,17 @@
 <?php
-	echo $_POST['username'];
+	include('configure.php');
+?>
+<?php
+	if (isset($_POST['btn'])) {
+
+		echo $_POST['id'].'<br>'; 
+		echo $_POST['username'].'<br>';
+		echo $_POST['age'].'<br>';
+		echo $_POST['contact'].'<br>';
+		echo $_POST['email'].'<br>';
+	}
+	else
+		echo "Welcome Guest!!"
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,7 +22,12 @@
 
 <body>
 	<form action="post.php" method="post">
+		Id: <input type="text" name="id" placeholder="Enter ID"> <br>
 		Name: <input type="text" name="username"> <br>
+		Age: <input type="text" name="age" placeholder="Enter age"> <br>
+		Contact: <input type="text" name="contact" placeholder="Enter contact number"> <br>
+		Email: <input type="text" name="email" placeholder="Enter email"> <br>
+
 		<input type="submit" name="btn">
 	</form>
 </body>
